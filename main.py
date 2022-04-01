e50c1a3ef2c5d462fcb8f741fb12183bd8ec2e04
wtl666wtl <2351090351@qq.com>
2022-04-01 17:47:52 +0800

    1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 from torch.utils.data import Dataset
 import csv
 
-batch_size= 256
+batch_size = 256
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 
@@ -19,14 +19,16 @@ class TextDataset(Dataset):
         self.batch_tokenized = tokenizer.batch_encode_plus(text, add_special_tokens=True,
                                                            max_length=64, truncation=True, pad_to_max_length=True)
         self.batch_tokenized2 = tokenizer.batch_encode_plus(text2, add_special_tokens=True,
-                                                                max_length=64, truncation=True, pad_to_max_length=True)
+                                                            max_length=64, truncation=True, pad_to_max_length=True)
 
     def __len__(self):
         return len(self.text)
 
     def __getitem__(self, item):
-        text = (torch.tensor(self.batch_tokenized["input_ids"][item]), torch.tensor(self.batch_tokenized["attention_mask"][item]),
-                torch.tensor(self.batch_tokenized2["input_ids"][item]), torch.tensor(self.batch_tokenized2["attention_mask"][item]))
+        text = (torch.tensor(self.batch_tokenized["input_ids"][item]),
+                torch.tensor(self.batch_tokenized["attention_mask"][item]),
+                torch.tensor(self.batch_tokenized2["input_ids"][item]),
+                torch.tensor(self.batch_tokenized2["attention_mask"][item]))
         if not self.test:
             return text, self.label[item]
         else:
@@ -35,7 +37,7 @@ class TextDataset(Dataset):
 
 def main():
     # input
-    reader = csv.reader(open("train.tsv","r",encoding="utf-8"), delimiter='\t')
+    reader = csv.reader(open("train.tsv", "r", encoding="utf-8"), delimiter='\t')
     train_text = []
     train_text2 = []
     test_text = []
@@ -46,7 +48,7 @@ def main():
             train_text.append(t1)
             train_text2.append(t2)
             label.append(int(l))
-    reader = csv.reader(open("test.tsv","r",encoding="utf-8"), delimiter='\t')
+    reader = csv.reader(open("test.tsv", "r", encoding="utf-8"), delimiter='\t')
     for id, t1, t2 in reader:
         if not id.isalpha():
             test_text.append(t1)
@@ -56,7 +58,7 @@ def main():
     train_set = TextDataset(test=False, text=train_text, text2=train_text2, label=label)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=1)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,
-                                                 num_workers=4, drop_last=True)
+                                               num_workers=4, drop_last=True)
     device = torch.device('cuda')
     model = Bert_model(2).to(device)
     optimizer = optim.Adam(model.parameters(), lr=1e-5)
@@ -98,10 +100,11 @@ def main():
     preds = preds.astype(int)
 
     # output
-    writer = csv.writer(open("submission.csv","w",encoding="utf-8"))
+    writer = csv.writer(open("submission.csv", "w", encoding="utf-8"))
     writer.writerow(("Id", "Category"))
     for i in range(len(preds)):
         writer.writerow((i, preds[i]))
+
 
 if __name__ == '__main__':
     main()
