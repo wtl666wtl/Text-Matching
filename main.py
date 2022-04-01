cd805f2435fc64f677e8da1ae288ffba9659a391
wtl666wtl <2351090351@qq.com>
2022-04-01 17:31:29 +0800

    1

---
+++ main.py
@@ -0,0 +1,111 @@
+import argparse
+import warnings
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from model import *
+from sklearn import metrics
+import transformers as tfs
+import numpy as np
+import os
+import copy
+from torch.utils.data import Dataset
+import csv
+
+batch_size= 256
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+class TextDataset(Dataset):
+    def __init__(self, test, text, text2, label=None):
+        self.test = test
+        self.text2 = text2
+        self.text = text
+        self.label = label
+        self.batch_tokenized = tokenizer.batch_encode_plus(text, add_special_tokens=True,
+                                                           max_length=64, truncation=True, pad_to_max_length=True)
+        self.batch_tokenized2 = tokenizer.batch_encode_plus(text2, add_special_tokens=True,
+                                                                max_length=64, truncation=True, pad_to_max_length=True)
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, item):
+        text = (torch.tensor(self.batch_tokenized["input_ids"][item]), torch.tensor(self.batch_tokenized["attention_mask"][item]),
+                torch.tensor(self.batch_tokenized2["input_ids"][item]), torch.tensor(self.batch_tokenized2["attention_mask"][item]))
+        if not self.test:
+            return text, self.label[item]
+        else:
+            return text, []
+
+
+def main():
+    # input
+    reader = csv.reader(open("train.tsv","r",encoding="utf-8"), delimiter='\t')
+    train_text = []
+    train_text2 = []
+    test_text = []
+    test_text2 = []
+    for id, t1, t2 in reader:
+        if not id.isalpha():
+            train_text.append(t1)
+            train_text2.append(t2)
+    reader = csv.reader(open("test.tsv","r",encoding="utf-8"), delimiter='\t')
+    for id, t1, t2 in reader:
+        if not id.isalpha():
+            test_text.append(t1)
+            test_text2.append(t2)
+
+    test_set = TextDataset(test=True, text=test_text, text2=test_text2)
+    train_set = TextDataset(test=False, text=train_text, text2=train_text2, label=labeled_label)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=1)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,
+                                                 num_workers=4, drop_last=True)
+    device = torch.device('cuda')
+    model = Bert_model(2).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-5)
+    criterion = nn.CrossEntropyLoss().to(device)
+
+    # train
+    batch_count = len(train_text) // batch_size
+    model.train()
+    for epoch in range(5):
+        print_avg_loss = 0
+        for batch_idx, ((x, mx, x2, mx2), label) in enumerate(train_loader):
+            x = x.to(device)
+            mx = mx.to(device)
+            x2 = x2.to(device)
+            mx2 = mx2.to(device)
+            label = label.to(device)
+            optimizer.zero_grad()
+            outputs, _ = model(x, mx, x2, mx2)
+            loss = criterion(outputs, label)
+            loss.backward()
+            optimizer.step()
+            print_avg_loss += loss.item()
+        print("Epoch: %d, Loss: %.4f" % ((epoch + 1), print_avg_loss / batch_count))
+
+    # test
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, ((x, mx, x2, mx2), _) in enumerate(test_loader):
+            x = x.to(device)
+            mx = mx.to(device)
+            x2 = x2.to(device)
+            mx2 = mx2.to(device)
+            label = label.to(device)
+            output, _ = model(x, mx, x2, mx2)
+            prob = F.softmax(output, dim=1)
+            conf, pred = prob.max(1)
+            preds = np.append(preds, pred.cpu().numpy())
+    preds = preds.astype(int)
+
+    # output
+    writer = csv.writer(open("submission.csv","r",encoding="utf-8"))
+    writer.writerow(("Id","Category"))
+    for i in range(len(preds)):
+        writer.writerow((i, preds[i]))
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+from torch import optim
+import transformers as tfs
+import math
+import numpy as np
+
+model_class, tokenizer_class, pretrained_weights = (tfs.BertModel, tfs.BertTokenizerFast, 'bert-base-uncased')
+tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+
+class Bert_model(nn.Module):
+    def __init__(self, k):
+        super(Bert_model, self).__init__()
+        self.bert = model_class.from_pretrained(pretrained_weights)
+        for param in self.bert.parameters():
+            param.requires_grad = True
+        self.dense = nn.Linear(768 * 2, k)
+
+    def forward(self, input_ids, attention_mask, input_ids2, attention_mask2):
+        _, pooled = self.bert(input_ids, attention_mask=attention_mask)
+        _, pooled2 = self.bert(input_ids2, attention_mask=attention_mask2)
+        tmp = (pooled, pooled2)
+        linear_output = self.dense(tmp)
+        return linear_output, tmp
