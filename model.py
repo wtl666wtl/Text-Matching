937a6b40c24211dc09b7b17416e7de52f9d32f9d
wtl666wtl <2351090351@qq.com>
2022-04-03 19:21:29 +0800

    1

--- model.py
+++ model.py
@@ -11,9 +11,10 @@ class Bert_model(nn.Module):
         self.bert = model_class.from_pretrained(pretrained_weights)
         for param in self.bert.parameters():
             param.requires_grad = True
+        self.dropout = nn.Dropout(p=0.1)
         self.dense = nn.Linear(768, k)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         _, pooled = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        linear_output = self.dense(pooled)
+        linear_output = self.dense(self.dropout(pooled))
         return linear_output, pooled
