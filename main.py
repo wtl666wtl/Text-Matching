253187b04f6662e91718e57a24fbddb77862421c
wtl666wtl <2351090351@qq.com>
2022-05-25 20:44:42 +0800

    1

--- main.py
+++ main.py
@@ -77,11 +77,13 @@ def main():
     model.train()
     for epoch in range(10):
         print_avg_loss = 0
-        for batch_idx, (x, label) in enumerate(train_loader):
+        for batch_idx, ((x, mx, tx), label) in enumerate(train_loader):
             x = x.to(device)
+            mx = mx.to(device)
+            tx = tx.to(device)
             label = label.to(device)
             optimizer.zero_grad()
-            outputs, _ = model(x)
+            outputs, _ = model(x, mx, tx)
             loss = criterion(outputs, label)
             loss.backward()
             optimizer.step()
@@ -92,9 +94,11 @@ def main():
     model.eval()
     preds = np.array([])
     with torch.no_grad():
-        for batch_idx, (x, _) in enumerate(test_loader):
+        for batch_idx, ((x, mx, tx), _) in enumerate(test_loader):
             x = x.to(device)
-            output, _ = model(x)
+            mx = mx.to(device)
+            tx = tx.to(device)
+            output, _ = model(x, mx, tx)
             prob = F.softmax(output, dim=1)
             conf, pred = prob.max(1)
             preds = np.append(preds, pred.cpu().numpy())
