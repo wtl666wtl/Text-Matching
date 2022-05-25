f3b4c01932d7e044875d5ef4cb774907ffbd4b4c
wtl666wtl <2351090351@qq.com>
2022-05-25 21:48:21 +0800

    1

--- naive_model.py
+++ naive_model.py
@@ -10,7 +10,7 @@ class naive_model(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
         self.mp = nn.MaxPool2d(2)
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(16 * 15 * 15, 256)
+        self.fc1 = nn.Linear(16 * 14 * 14, 256)
         self.fc2 = nn.Linear(256, 64)
         self.fc3 = nn.Linear(64, 2)
 
@@ -20,7 +20,7 @@ class naive_model(nn.Module):
         x = x.unsqueeze(1) # 128 * 1 * 64 * 64
         #print(x.size())
         out = self.relu(self.mp(self.conv1(x))) # 1 * 64 * 64 -> 6 * 32 * 32
-        out = self.relu(self.mp(self.conv2(out))) # 6 * 32 * 32 -> 16 * 15 * 15
+        out = self.relu(self.mp(self.conv2(out))) # 6 * 32 * 32 -> 16 * 14 * 14
         out = out.view(in_size, -1)
         out = self.relu(self.fc1(out))
         out = self.relu(self.fc2(out))
