44feda9a76d5558ec456176ae4ccb4545a47e9c7
wtl666wtl <2351090351@qq.com>
2022-06-03 00:21:32 +0800

    1

---
+++ XLNet.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+import transformers as tfs
+
+model_class, tokenizer_class, pretrained_weights = (tfs.XLNetModel, tfs.XLNetTokenizer, 'xlnet-base-cased')
+tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+
+class XLNet(nn.Module):
+    def __init__(self, k):
+        super(XLNet, self).__init__()
+        self.bert = model_class.from_pretrained(pretrained_weights)
+        for name, param in self.net.named_parameters():
+            if 'layer.11' in name or 'layer.10' in name or 'layer.9' in name or 'layer.8' in name or 'pooler.dense' in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+        self.dropout = nn.Dropout(p=0.2)
+        self.dense = nn.Linear(768, k)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        _, pooled, hidden = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        linear_output = self.dense(self.dropout(pooled))
+        return linear_output, pooled
---
+++ xlnet_main.py
@@ -0,0 +1,151 @@
+import random
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+#from model import *
+from XLNet import *
+import numpy as np
+from torch.utils.data import Dataset
+import csv
+from transformers import get_linear_schedule_with_warmup
+
+Epoch = 3
+batch_size = 32
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+class TextDataset(Dataset):
+    def __init__(self, test, text, text2, label=None):
+        self.test = test
+        self.label = label
+        self.text = text
+        self.text2 = text2
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, item):
+        t1 = self.text[item]
+        t2 = self.text2[item]
+        if len(t1) > 200:
+            t1 = t1[:200]
+        if len(t2) > 200:
+            t2 = t2[:200]
+        encode = tokenizer.encode_plus(t1, t2, add_special_tokens=True,
+                                        max_length=256, truncation='longest_first',
+                                        pad_to_max_length=True, return_tensors='pt')
+        text = (encode['input_ids'].squeeze(0), encode['attention_mask'].squeeze(0),
+                encode['token_type_ids'].squeeze(0))
+        if not self.test:
+            return text, self.label[item]
+        else:
+            return text, []
+
+
+def main():
+    # input
+    reader = csv.reader(open("train.tsv", "r", encoding="utf-8"), delimiter='\t')
+    train_text = []
+    train_text2 = []
+    test_text = []
+    test_text2 = []
+    label = []
+    for id, t1, t2, l in reader:
+        if not id.isalpha():
+            train_text.append(t1)
+            train_text2.append(t2)
+            label.append(int(l))
+            train_text.append(t2)
+            train_text2.append(t1)
+            label.append(int(l))
+            """
+            train_text.append(t1)
+            train_text2.append(t1)
+            label.append(1)
+            train_text.append(t2)
+            train_text2.append(t2)
+            label.append(1)
+            train_text.append(t1)
+            train_text2.append(train_text2[random.randint(0, len(train_text2)-4)])
+            label.append(0)
+            train_text.append(t2)
+            train_text2.append(train_text[random.randint(0, len(train_text)-5)])
+            label.append(0)
+            """
+    reader = csv.reader(open("test.tsv", "r", encoding="utf-8"), delimiter='\t')
+    for id, t1, t2 in reader:
+        if not id.isalpha():
+            test_text.append(t1)
+            test_text2.append(t2)
+
+    test_set = TextDataset(test=True, text=test_text, text2=test_text2)
+    train_set = TextDataset(test=False, text=train_text, text2=train_text2, label=label)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=1)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,
+                                               num_workers=4, drop_last=True)
+    device = torch.device('cuda')
+    model = XLNet(2).to(device)
+    #model = Bert_model(2).to(device)
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {
+            'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+            'weight_decay': 0.01
+        },
+        {
+            'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+            'weight_decay': 0.0
+        }
+    ]
+    total_steps = len(train_loader) * Epoch
+    optimizer = optim.AdamW(optimizer_grouped_parameters, lr=2e-5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.85, patience=0)
+    #scheduler = get_linear_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=0.05 * total_steps,
+    #                                            num_training_steps=total_steps)
+    criterion = nn.CrossEntropyLoss().to(device)
+    print("Start training!")
+
+    # train
+    batch_count = len(train_text) // batch_size
+    model.train()
+    for epoch in range(Epoch):
+        print_avg_loss = 0
+        for batch_idx, ((x, mx, tx), label) in enumerate(train_loader):
+            x = x.to(device)
+            mx = mx.to(device)
+            tx = tx.to(device)
+            label = label.to(device)
+            optimizer.zero_grad()
+            outputs, _ = model(x, mx, tx)
+            loss = criterion(outputs, label)
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)
+            optimizer.step()
+            print_avg_loss += loss.item()
+        scheduler.step(loss)
+        print("Epoch: %d, Loss: %.4f" % ((epoch + 1), print_avg_loss / batch_count))
+
+    # test
+    model.eval()
+    preds = np.array([])
+    with torch.no_grad():
+        for batch_idx, ((x, mx, tx), _) in enumerate(test_loader):
+            x = x.to(device)
+            mx = mx.to(device)
+            tx = tx.to(device)
+            output, _ = model(x, mx, tx)
+            prob = F.softmax(output, dim=1)
+            conf, pred = prob.max(1)
+            preds = np.append(preds, pred.cpu().numpy())
+    preds = preds.astype(int)
+
+    # output
+    writer = csv.writer(open("xlnet_submission.csv", "w", encoding="utf-8"))
+    writer.writerow(("Id", "Category"))
+    for i in range(len(preds)):
+        writer.writerow((i, preds[i]))
+
+
+if __name__ == '__main__':
+    main()
