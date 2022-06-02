660f47ffbb20e470889a8ff0913daa499d0c6bba
wtl666wtl <2351090351@qq.com>
2022-06-03 00:44:18 +0800

    1

--- XLNet.py
+++ XLNet.py
@@ -18,6 +18,6 @@ class XLNet(nn.Module):
         self.dense = nn.Linear(768, k)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
-        last_hidden = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        last_hidden = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids).last_hidden_state
         linear_output = self.dense(self.dropout(last_hidden))
         return linear_output, last_hidden
