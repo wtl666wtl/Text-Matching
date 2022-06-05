63fddda26c554c7356d0639ea8a8a16e91ee5a3f
wtl666wtl <2351090351@qq.com>
2022-06-05 20:32:57 +0800

    1

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def main():
         }
     ]
     total_steps = len(train_loader) * Epoch
-    optimizer = optim.AdamW(optimizer_grouped_parameters, lr=3.5e-5)
+    optimizer = optim.AdamW(optimizer_grouped_parameters, lr=1.5e-5)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.85, patience=0)
     #scheduler = get_linear_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=0.05 * total_steps,
     #                                            num_training_steps=total_steps)
